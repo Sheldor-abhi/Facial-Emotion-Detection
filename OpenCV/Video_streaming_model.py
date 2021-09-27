195de64d2557f12e37183a0ffdd5640ee2fabf87
Sheldor-abhi <59531658+Sheldor-abhi@users.noreply.github.com>
2021-09-27 21:41:00 +0530

    removing tf part

--- OpenCV/Video_streaming_model.py
+++ OpenCV/Video_streaming_model.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, Response
 import cv2
-import os
-os.environ['CUDA_VISIBLE_DEVICES']='-1'
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from keras.preprocessing import image
-# model = keras.models.load_model('fer_60_epochs.h5')
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 app = Flask(__name__)
@@ -15,24 +10,11 @@ app = Flask(__name__)
 def gen_frames():  
     while True:
         success,frame=cap.read()
+        frame=cv2.resize(frame,(640,480))
         gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         face=face_cascade.detectMultiScale(gray,1.3,5)
         for (x,y,w,h) in face:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray=gray[y:y+w,x:x+h] #cropping region of interest 
-            roi_gray=cv2.resize(roi_gray,(48,48))
-            img_pixels = image.img_to_array(roi_gray)
-            img_pixels = np.expand_dims(img_pixels, axis = 0)
-            img_pixels /= 255
-
-            # predictions = model.predict(img_pixels)
-            #find max indexed array
-            # max_index = np.argmax(predictions[0])
-
-            # emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-            # predicted_emotion = emotions[max_index]
-
-            # cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
         if not success:
             break
         else:
@@ -51,4 +33,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",debug=False)
